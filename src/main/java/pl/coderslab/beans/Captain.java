82338dbc0171d3cc5a887f876b84dcc899cb86ac
SebastianBanasiewicz <banasiewiczsebastian@gmail.com>
2022-07-09 20:31:53 +0200

    Spring java

---
+++ src/main/java/pl/coderslab/beans/Captain.java
@@ -0,0 +1,11 @@
+package pl.coderslab.beans;
+
+import lombok.Data;
+
+@Data
+public class Captain {
+
+    public void startSailing() {
+        System.out.println("Start sailing.");
+    }
+}
---
+++ src/main/java/pl/coderslab/beans/Ship.java
@@ -0,0 +1,16 @@
+package pl.coderslab.beans;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Ship {
+
+    Captain captain;
+
+}
